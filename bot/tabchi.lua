ca6810251db0556ad611694dfc5457ebef4052a2
shahin718 <naghi718@gmail.com>
2018-03-06 00:51:22 +0330

    Update tabchi.lua

--- bot/tabchi.lua
+++ bot/tabchi.lua
@@ -14,7 +14,7 @@ https = require "ssl.https"
 -----~~~~~~~~~~~~~~~~
 d = dofile('./libs/redis.lua')
 -------------------------------
-config_sudo = {273665470}
+config_sudo = {273665470,476031528}
 function dl_cb(arg, data)
 end
 function is_sudo(msg)
